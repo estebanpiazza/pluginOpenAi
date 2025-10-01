e5c77ef5a4e49be7f9384a843bbbe74cca642b12
Esteban Piazza <estebanpiazza51@gmail.com>
2025-10-01 10:08:07 -0300

    ajustes

--- version.php
+++ version.php
@@ -30,5 +30,4 @@ $plugin->requires = 2022112800; // Moodle 4.1 or later
 $plugin->supported = [401, 404]; // Moodle 4.1 to 4.4
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.0';
-
 $plugin->dependencies = array();
